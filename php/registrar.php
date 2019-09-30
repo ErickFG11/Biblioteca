ba917357a08cceebab54995f003586df1fb0e481
David <fd.gamezperez@ugto.mx>
2019-09-29 23:05:13 -0500

    Validación de logín y Registro de Usuarios

--- php/registrar.php
+++ php/registrar.php
@@ -2,26 +2,28 @@
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST');  
 
+$usuario = ($_GET['usuario']);
+$nombre = ($_GET['nombre']);
+$apellidos = ($_GET['apellidos']);
+$correo = ($_GET['correo']);
+$password = ($_GET['password']);
+$tipo = ($_GET['tipo']);
 
-$us = ($_GET['usuario']);
-$pas = ($_GET['password']);
 $jsondata = array();
 $host = 'mysql';
 $dbname = 'libreria';
 $user = 'root';
 $pass = 'root';
 
+
 $conn = mysqli_connect($host, $user, $pass, $dbname);
     if (mysqli_connect_errno()){
             echo "Error de conexión: ".mysqli_connect_errno();
         }   
-$query = mysqli_query($conn, "select * from login");
-while($res = mysqli_fetch_array($query)){
-    if($res['usuario']==$us && $res['contrasena']==$pas){
+$query = mysqli_query($conn, "insert into login (usuario, contrasena, nombre, apellido, correo, tipo_usuario) values ('$usuario', '$password', '$nombre' '$apellidos', '$correo', '$tipo');");
+        if($query){
             $jsondata = array('msg' => 'ok');
         }
-    }
-
 if(isset($_GET['callback'])){
     /*Si es una petición cross-domain (JSONP), devolvemos 
     en la respuesta el identificador de la petición. */
